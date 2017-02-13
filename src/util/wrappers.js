a798dfc865e4b11330954d282df0cd99e5fbc904
LAPTOP-E97B4KVK\joshq <joshquin@gmail.com>
2017-02-12 21:47:44 -0500

    Changed auth redirect route to home from signup.

--- src/util/wrappers.js
+++ src/util/wrappers.js
@@ -4,7 +4,7 @@ import { routerActions } from 'react-router-redux'
 export const UserIsAuthenticated = UserAuthWrapper({
   authSelector: state => state.user.data,
   redirectAction: routerActions.replace,
-  failureRedirectPath: '/signup', // '/login' by default.
+  failureRedirectPath: '/', // '/login' by default.
   wrapperDisplayName: 'UserIsAuthenticated'
 })
 
