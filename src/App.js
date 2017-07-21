db173836da86f4aa9393fc99bacc1b11bd731f66
baldwinjj <baldwinjj@g.cofc.edu>
2017-07-20 22:50:17 -0400

    Fix Firefox menu bug causing an unwanted line break.
    Firefox has a known bug where floated boxes can cause line breaks.
    In this case, the right floated menu items are creating a line break
    in the menu and obscuring some of the content. Create a simple fix by
    listing the floated element first in the JSX so thatthe browser
    positions it before rendering the elements still in the page flow.

--- src/App.js
+++ src/App.js
@@ -38,11 +38,11 @@ class App extends Component {
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
-          <Link to="/" className="pure-menu-heading pure-menu-link">Truffle Box</Link>
           <ul className="pure-menu-list navbar-right">
             <OnlyGuestLinks />
             <OnlyAuthLinks />
           </ul>
+          <Link to="/" className="pure-menu-heading pure-menu-link">Truffle Box</Link>
         </nav>
 
         {this.props.children}
